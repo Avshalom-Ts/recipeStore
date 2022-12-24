1b52d7f0af450b51e1b6480a9d7cf6a84cbc7bf8
Azdevpp <Azdevpp@gmail.com>
2022-12-24 17:54:02 +0200

    Added Misibg code from lecture 122 curent lecture 157

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,27 +1,34 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
-export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Reciep', 'This is simple Test',
-            'https://www.acouplecooks.com/wp-content/uploads/2019/11/Recipes-Header-1.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Freis', 20)
-            ]),
-        new Recipe(
-            'Another Test Reciep', 'This is simple Test',
-            'https://www.acouplecooks.com/wp-content/uploads/2019/11/Recipes-Header-1.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-    ];
-    
-    getRecipe() {
-        return this.recipes.slice();
-    }
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Reciep',
+      'This is simple Test',
+      'https://www.acouplecooks.com/wp-content/uploads/2019/11/Recipes-Header-1.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Freis', 20)]
+    ),
+    new Recipe(
+      'Another Test Reciep',
+      'This is simple Test',
+      'https://www.acouplecooks.com/wp-content/uploads/2019/11/Recipes-Header-1.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipe() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }

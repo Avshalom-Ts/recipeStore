a8ba6f12cbe5d936d9b1b43f88bc127b4cd07bb3
Azdevpp <Azdevpp@gmail.com>
2022-12-24 18:42:15 +0200

    Generated the recipe start compnent and Added to the childen rout of recipe route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
   {
@@ -12,6 +13,7 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    children: [{ path: '', component: RecipeStartComponent }],
   },
   {
     path: 'shopping-list',
